a39f541d173c01039ab14d2d5e37ada92f9919fb
Madan Purohit <mspurohit09@gmail.com>
2020-09-26 16:59:21 +0530

    setup first controlles

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 const express=require('express');
 const router=express.Router();
+const homeController=require('../controllers/home_controllers');
+router.get('/',homeController.home);
 console.log('router is loaded');
 module.exports=router;

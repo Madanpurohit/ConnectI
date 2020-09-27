54ddb271c6deddb29651670ec29a245bc0cc34e0
Madan Purohit <mspurohit09@gmail.com>
2020-09-27 09:29:24 +0530

    install ejs and setup view engine

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app=express();
 const port=8000;
 //setup routes 
 app.use('/',require('./routes'));
+
+//setup view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err)
     {

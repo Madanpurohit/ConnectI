eda0e765487f96913678d85e51da3f94cc4ff062
Madan Purohit <mspurohit09@gmail.com>
2020-10-22 13:33:02 +0530

    display the post on home page

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,8 +1,12 @@
+const Post=require('../models/post');
 module.exports.home=function(req,res)
 {
     // console.log(req.cookies);
     // res.cookie('user-name',25)
-    return res.render('home',{
-        title:"Home"
-    });
+    Post.find({},function(err,posts){
+        return res.render('home',{
+            title:"codial | home",
+            posts:posts
+        });
+    })
 }
